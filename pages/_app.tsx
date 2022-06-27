25aaa75bc8e5845f3a69d465d6797956541e2889
Jay Joo <jj534@cornell.edu>
2022-06-27 01:03:12 -0400

    Add scroll restoration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,22 +4,16 @@ import AppDefaultHead from "components/glue/AppDefaultHead"
 import AppShell from "components/glue/AppShell"
 import MantineConfigProvider from "components/glue/MantineConfigProvider"
 import SWRProvider from "components/glue/SWRProvider"
+import { useScrollRestoration } from "hooks/glue/useScrollRestoration"
 import { SessionProvider } from "next-auth/react"
 import { AppProps } from "next/app"
-import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
 import "react-loading-skeleton/dist/skeleton.css"
 import "styles/glue/reset.css"
 
 const App = ({ Component, pageProps }: AppProps) => {
-  // TODO: find a better fix
-  // this wouldn't allow for SSR at all in the entire app
-  // const [isMounted, setIsMounted] = useState<boolean>(false)
-
-  // useEffect(() => {
-  //   setIsMounted(true)
-  // }, [])
-
-  // if (!isMounted) return null
+  const router = useRouter()
+  useScrollRestoration(router)
 
   return (
     <SessionProvider session={pageProps.session}>
