1a0d280f4bbb467b866516c0c78649968bd89fa0
Jay Joo <jj534@cornell.edu>
2022-08-11 13:54:26 -0400

    fix forward ref error

--- components/glue/ClickableContainer.tsx
+++ components/glue/ClickableContainer.tsx
@@ -1,24 +1,30 @@
 import { Container, ContainerProps } from "@mantine/core"
 import React from "react"
 
-const ClickableContainer = ({ children, ...rest }: ContainerProps) => {
-  return (
-    <Container
-      sx={(theme) => ({
-        [`@media (min-width: ${theme.breakpoints.xs}px)`]: {
-          cursor: "pointer",
-          borderRadius: "8px",
+const ClickableContainer = React.forwardRef<HTMLDivElement, ContainerProps>(
+  (props, ref) => {
+    const { children, ...rest } = props
+    return (
+      <Container
+        ref={ref}
+        sx={(theme) => ({
+          [`@media (min-width: ${theme.breakpoints.xs}px)`]: {
+            cursor: "pointer",
+            borderRadius: "8px",
 
-          "&:hover": {
-            background: theme.colors.gray[0],
+            "&:hover": {
+              background: theme.colors.gray[0],
+            },
           },
-        },
-      })}
-      {...rest}
-    >
-      {children}
-    </Container>
-  )
-}
+        })}
+        {...rest}
+      >
+        {children}
+      </Container>
+    )
+  }
+)
+
+ClickableContainer.displayName = "ClickableContainer"
 
 export default ClickableContainer
