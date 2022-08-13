781162c6d221988ed7b2b823f7b6b1f60db71fea
Jay Joo <jj534@cornell.edu>
2022-08-13 12:43:25 -0400

    add private route handling

--- components/glue/PageContainer.tsx
+++ components/glue/PageContainer.tsx
@@ -1,15 +1,32 @@
 import { Container } from "@mantine/core"
+import { useSession } from "next-auth/react"
 import Head from "next/head"
-import React from "react"
+import { useRouter } from "next/router"
+import React, { useEffect } from "react"
 import Flex from "./Flex"
 
 interface IPageContainerProps {
   children: React.ReactNode
   variant: "mobile-only"
   title?: string
+  isPrivate?: boolean
 }
 
-const PageContainer = ({ children, title, variant }: IPageContainerProps) => {
+const PageContainer = ({
+  children,
+  title,
+  variant,
+  isPrivate = false,
+}: IPageContainerProps) => {
+  const router = useRouter()
+  const { status } = useSession()
+
+  useEffect(() => {
+    if (isPrivate && status === "unauthenticated") {
+      router.push("/api/auth/signin")
+    }
+  }, [isPrivate, status])
+
   if (variant === "mobile-only") {
     return (
       <>
