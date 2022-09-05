8f28bfbb5d934e38a50115ae8d0c50e0ad891d9e
Jay Joo <jj534@cornell.edu>
2022-09-05 13:58:22 -0400

    initialize GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,10 +5,9 @@ import AppDefaultHead from "components/glue/AppDefaultHead"
 import AppShell from "components/glue/AppShell"
 import MantineConfigProvider from "components/glue/MantineConfigProvider"
 import SWRProvider from "components/glue/SWRProvider"
-import { useScrollRestoration } from "hooks/glue/useScrollRestoration"
 import { SessionProvider } from "next-auth/react"
 import { AppProps } from "next/app"
-import { useRouter } from "next/router"
+import ReactGA from "react-ga4"
 import "react-loading-skeleton/dist/skeleton.css"
 import { ThemeProvider } from "styled-components"
 import "styles/glue/reset.css"
@@ -18,6 +17,8 @@ const App = ({ Component, pageProps }: AppProps) => {
   // const router = useRouter()
   // useScrollRestoration(router)
   const theme = useMantineTheme()
+
+  ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID)
 
   return (
     <SessionProvider session={pageProps.session}>
