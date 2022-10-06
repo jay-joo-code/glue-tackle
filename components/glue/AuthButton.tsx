99ec8f5315b880297f36e9d624e8019f4f1b8a85
Jay Joo <jj534@cornell.edu>
2022-10-06 12:26:33 -0400

    remove private navs

--- components/glue/AuthButton.tsx
+++ components/glue/AuthButton.tsx
@@ -7,10 +7,10 @@ const AuthButton = () => {
   const { status, data } = useSession()
 
   const PRIVATE_NAV = [
-    {
-      label: "My tasks",
-      href: "/tasks/my-tasks",
-    },
+    // {
+    //   label: "My tasks",
+    //   href: "/tasks/my-tasks",
+    // },
   ]
 
   if (status !== "authenticated") {
@@ -43,7 +43,7 @@ const AuthButton = () => {
           </MenuItem>
         ))}
 
-        <Menu.Divider />
+        {/* <Menu.Divider /> */}
 
         <MenuItem onClick={() => signOut({ redirect: false })} color="red">
           Sign out
