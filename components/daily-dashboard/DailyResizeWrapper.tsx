bfb7d3488da6145a1e687a2bb942f7ad2feaeb29
Jay Joo <jj534@cornell.edu>
2022-10-09 10:20:15 -0400

    persist daily sprint width

--- components/daily-dashboard/DailyResizeWrapper.tsx
+++ components/daily-dashboard/DailyResizeWrapper.tsx
@@ -1,12 +1,29 @@
 import { Container } from "@mantine/core"
+import useGlueLocalStorage from "hooks/glue/useGlueLocalStorage"
 import useIsDevice from "hooks/glue/useIsDevice"
-import React, { useState } from "react"
+import { useEffect, useState } from "react"
 import { Resizable } from "react-resizable"
 import DailyDashboard from "./DailyDashboard"
 
 const DailyResizeWrapper = () => {
-  const [width, setWidth] = useState<number>(600)
+  const [persistedWidth, setPersistedWidth] = useGlueLocalStorage({
+    key: "daily-sprint-width",
+    defaultValue: null,
+  })
+  const [width, setWidth] = useState<number>(0)
   const { isMobile } = useIsDevice()
+
+  useEffect(() => {
+    if (persistedWidth !== undefined && width === 0) {
+      setWidth(persistedWidth)
+    }
+  }, [persistedWidth])
+
+  useEffect(() => {
+    if (width !== 0) {
+      setPersistedWidth(width)
+    }
+  }, [width])
 
   const handleResize = (_, { size }) => {
     setWidth(size.width)
